b02df092d602ec5e83435c24f8924c54eb8b30f6
marianhoc <copesmariano@gmail.com>
2017-06-25 13:28:42 -0300

    manage/Rooms scrapping resservation

--- GetRoomID.py
+++ GetRoomID.py
@@ -1,31 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-reservation = "https://inbox.hostelworld.com/booking/view/293725228"
 
-with requests.Session() as c:
-    url = "https://inbox.hostelworld.com/inbox/trylogin.php"
-    id = "273910"
-    user = "minihotel"
-    password = "Fede@2001"
+login_form_action = "https://inbox.hostelworld.com/inbox/trylogin.php"
+reservation_page = "https://inbox.hostelworld.com/booking/view/"
 
+def getToken():
     login = requests.get("https://inbox.hostelworld.com/inbox/index.php")
     soup = BeautifulSoup(login.text, 'lxml')
+    return soup.find("form").findChildren('input')[0].get('value')
 
-    formToken = soup.find("form").findChildren('input')[0].get('value')
-
-    login_data = {'formToken':formToken,
+def getLogin_data(id, user, password):
+    login_data = {'formToken': getToken(),
                   'SessionLanguage': 'English',
                   'HostelNumber': id,
                   'Username': user,
                   'Password': password}
+    return login_data
 
-    c.post(url, data=login_data, headers={"Referer":"https://inbox.hostelworld.com/inbox/index.php"})
+def getReservationPage(id,user, password, reservation_number):
+    with requests.Session() as c:
+        login_data = getLogin_data(id, user, password)
+        c.post(login_form_action, data=login_data, headers={"Referer":"https://inbox.hostelworld.com/inbox/index.php"})
 
-    manage_rooms = "https://inbox.hostelworld.com/manage/rooms"
-    rooms_rates = c.post(manage_rooms).json()
-    # soup_of_rooms = BeautifulSoup(rooms_rates.content, 'lxml')
-    print rooms_rates
-
-    # page = c.get(reservation)
-    # print page.content
+        return c.get(reservation_page + str(reservation_number)).content
--- main.py
+++ main.py
@@ -1,55 +1,69 @@
-import requests
 from bs4 import BeautifulSoup
-# from BeautifulSoup import *
-html = ""
-guest = []
+import GetRoomID
 
+
+def import_reservation(gds, guest):
+    xml =   '<Bookings><Authentication username="bframe" password="bframe" />' \
+            '<Hotel id="'+ gds +'" />' \
+            '<Booking id="" type="Book" createDateTime="" source="HW">' \
+            '<RoomStay roomTypeID="Dorm6" ratePlanID="" roomTypeName="">' \
+            '<StayDate arrival="'+ guest['check_in'] +'" departure="" />' \
+            '<RoomCount NumberOfUnits="'+ guest['adults'] +'" />' \
+            '<GuestCount adult="'+ guest['adults'] +'" child="0" baby="0" />' \
+            '<PerDayRates CurrencyCode="USD">' \
+            '<PerDayRate stayDate="" baseRate="" />' \
+            '<PerDayRate stayDate="" baseRate="" />' \
+            '<PerDayRate stayDate="" baseRate="" /></PerDayRates>' \
+            '<Total AmountAfterTaxes="" CurrencyCode="" />' \
+            '<GuestNames><Name givenName="'+ str(guest['name']).split()[0] +'" surname="'+ str(guest['name']).split()[1] +'" /></GuestNames>' \
+            '</RoomStay><PrimaryGuest>' \
+            '<Name givenName="'+ str(guest['name']).split()[0] +'" surname="'+ str(guest['name']).split()[1] +'" /><Address Street="" Zip="" City="" /><Country CountryName="" iso2="" iso3="" /><Language iso2="" />' \
+            '<Email>'+ guest['email'] +'</Email><Phone>'+ guest['phone'] +'</Phone><Fax></Fax>' \
+            '<CreditCard Type="" Number="" NameOnCard="" Expirationdate="" />' \
+            '</PrimaryGuest><Remarks></Remarks>' \
+            '<ResGlobalInfo><Timespan arrival="" departure="" /><Total AmountAfterTaxes="' + guest['total'] + '" CurrencyCode="USD" />' \
+            '</ResGlobalInfo></Booking></Bookings>'
+
+    return xml
 
 with open("C:/Users/Mariano/Desktop/test-scrapper/georgia.html", 'r')as input:
     html = input.read()
 
+# hostel_id = raw_input("hostel ID = ")
+# user = raw_input("user = ")
+# password = raw_input("password = ")
+# res_number = raw_input("reservation number = ")
+
+
+# html = GetRoomID.getReservationPage(hostel_id, user, password, res_number)
+
 soup = BeautifulSoup(html, 'lxml')
-
-ulChildren = soup.find("div", {"class", "content"})
-# ulChildren = BeautifulSoup(ulChildren.finfChildren("ul"), 'lxml')
-
-# ========Customer details div
-customerDetail = ""
-for line in soup.find_all("ul", {"class", "customer-details"}):
-    customerDetail = customerDetail + str(line)
-
-customerDetail = BeautifulSoup(customerDetail, 'lxml')
+guest = {}
+guest['name'] = soup.find("ul", {"class", "customer-details"}).findChildren("li")[1].text
+guest['email'] = soup.find("ul", {"class", "customer-details"}).findChildren("li")[3].text
+guest['phone'] = soup.find("ul", {"class", "customer-details"}).findChildren("li")[5].text
 
 # =============  payment div =========================
 payment = soup.find('div', {'prices-total'})
 payment = BeautifulSoup(str(payment.findChildren("li")), 'lxml')
 
-
-for x in range(1, len(customerDetail.find_all("li")), 2):
-    guest.append(customerDetail.find_all("li")[x].text)
-
-print "---------------------------------------"
-#  ---------- - -----NAME------------- - - -----"
-print guest[0]
-# -=-=-=-==-=-=EMAIL -=-=-=-=-=-=-
-print str(guest[1]).strip(" ")
-
-# ---------------telephone
-print guest[2]
+ulChildren = soup.find("div", {"class", "content"})
+guest['check_in'] = ulChildren.findChildren('ul')[1].findChildren('li')[0].text
+guest['room_type'] = ulChildren.findChildren('ul')[1].findChildren('li')[2].text.encode('utf-8')[53:-101]
+guest['nights'] = len(ulChildren.findChildren('ul')) - 1
+guest['adults'] = ulChildren.findChildren('ul')[1].findChildren('li')[3].text
+guest['total'] = str(payment.findChildren('li')[7].text).split(" ")[1]
 
 
-print "- - - - - - - - - -check in - - - - - - - - - - - - -"
-# -------------------- check in ---------------
-print ulChildren.findChildren('ul')[1].findChildren('li')[0].text
+line = "{},{},{},{},{},{},{},{},{}".format(str(guest['name']).split()[0],
+                                        str(guest['name']).split()[1],
+                                            guest['email'],
+                                            guest['phone'],
+                                            guest['check_in'],
+                                            guest['room_type'],
+                                            guest['nights'],
+                                            guest['adults'],
+                                            guest['total'])
+print line
 
-print "- - - - - - - - - NIGHTS- - - - - - - - - - - - - -"
-# --------------------NIGHTS ---------------
-print len(ulChildren.findChildren('ul')) - 1
-
-print "- - - - - - - - - Adults- - - - - - - - - - - - - -"
-# --------------------NIGHTS ---------------
-print ulChildren.findChildren('ul')[1].findChildren('li')[3].text
-
-# --$-$-$-$-$-$-$-$-$- TOTAL ---------------
-print "- - - - - - - - - TOTAL- - - - - - - - - - - - - -"
-print str(payment.findChildren('li')[7].text).split(" ")[1]
+print import_reservation("fedetest", guest)
